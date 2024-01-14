4f4e367cd8c9fa272c77535240a93977e2fed28c
Raphael Monte <raphaeldetarso@gmail.com>
2024-01-14 11:28:50 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Raphael Monte on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
