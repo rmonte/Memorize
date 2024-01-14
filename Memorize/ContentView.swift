ab67660dcceafddaa144422638292147aa1239da
Raphael Monte <raphaeldetarso@gmail.com>
2024-01-14 12:08:03 -0300

    Togglabe isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,10 +11,12 @@ struct ContentView: View {
     var body: some View {
         
         HStack() {
+            CardView()
+            CardView()
             CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            CardView(isFaceUp: true)
+            
+            
         }
         .foregroundColor(.orange)
         .padding()
@@ -22,19 +24,21 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     
     var body: some View {
-        ZStack() {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
